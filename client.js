977db220f265c7f56eeaa3aca21d1b2ec6079373
PranveraGashi <121582082+Pranvera21@users.noreply.github.com>
2025-11-15 15:08:24 +0100

    fixes

--- client.js
+++ client.js
@@ -13,47 +13,45 @@ rlRole.question("Zgjidhni rolin tuaj (super/admin): ", (roleInput) => {
     const role = roleInput.trim().toLowerCase();
     let clientRole = role;
     rlRole.close();
-const client = new net.Socket();
-
-client.connect(PORT, HOST, () => {
-    console.log(' U lidhët me serverin me sukses!');
-    client.write(`/role ${clientRole}`);
-    client.write('Përshëndetje nga klienti!');
     
+    const client = new net.Socket();
 
-});
-
-client.on('data', (data) => {
-    console.log(' Mesazh nga serveri:', data.toString());
-});
-
-client.on('close', () => {
-    console.log(' Lidhja me serverin u mbyll.');
-});
-
-
-client.on('error', (err) => {
-    console.error(' Gabim:', err.message);
-});
-const rl = readline.createInterface({
-   input: process.stdin,
-    output: process.stdout
-});
-
-rl.on("line", (input) => {
-    if (role === "super") {
-        client.write(input);
-    } 
-    else if (role === "admin") {
+    client.connect(PORT, HOST, () => {
+        console.log(' U lidhët me serverin me sukses!');
      
-        const allowed = ["/list", "/read", "/upload", "/download", "/delete", "/search","/info"];
-        const cmd = input.split(" ")[0];
-        if (allowed.includes(cmd)) {
-            client.write(input);
-        } else {
-            console.log(" Komandë e ndaluar për admin. Lejohet vetëm:", allowed.join(", "));
-        }
-    }
-});
+        client.write(`/role ${clientRole}\n`);
+        
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
 
+        rl.on("line", (input) => {
+            if (role === "super") {
+                client.write(input + '\n');
+            } 
+            else if (role === "admin") {
+                const allowed = ["/list", "/read", "/upload", "/download", "/delete", "/search", "/info"];
+                const cmd = input.split(" ")[0];
+                if (allowed.includes(cmd)) {
+                    client.write(input + '\n');
+                } else {
+                    console.log(" Komandë e ndaluar për admin. Lejohet vetëm:", allowed.join(", "));
+                }
+            }
+        });
+
+        client.on('close', () => {
+            console.log(' Lidhja me serverin u mbyll.');
+            rl.close();
+        });
+    });
+
+    client.on('data', (data) => {
+        console.log(' Mesazh nga serveri:', data.toString());
+    });
+
+    client.on('error', (err) => {
+        console.error(' Gabim:', err.message);
+    });
 });
